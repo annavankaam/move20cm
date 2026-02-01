fe31d13f61b3bde65788c2f25bb2c40f38c5ab69
annavankaam <anne.van.kaam@icloud.com>
2026-02-01 21:04:01 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,8 @@
+maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+basic.pause(1000)
+basic.pause(500)
+maqueen.motorStop(maqueen.Motors.All)
+basic.forever(function () {
+	
+})

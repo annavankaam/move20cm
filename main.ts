bfa45f1852a30fecd58651e71b61d778943a7269
annavankaam <anne.van.kaam@icloud.com>
2026-02-06 16:01:37 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -3,13 +3,67 @@ IR.IR_init()
 let voor = 253
 let achter = 125
 let links = 31
-let rechts = 17
+let rechts = 6
+let stop = 221
 basic.forever(function () {
 	
 })
 basic.forever(function () {
-    IR2 = IR.IR_read()
+    if (IR2 == voor) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    }
+    if (IR2 == achter) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (IR2 == links) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(700)
+    }
+    if (IR2 == rechts) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        basic.pause(700)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . . . .
+            `)
+        if (IR2 == stop) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # . # .
+                . # # # .
+                . . . . .
+                `)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+        }
+    }
 })
 basic.forever(function () {
-    basic.showNumber(IR2)
+    IR2 = IR.IR_read()
 })
